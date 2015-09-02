8c12ba3ced63186985007a2d0ee43ffaa5ad6d3b
Juan Luis Baptiste <juan.baptiste@gmail.com>
2015-09-01 22:55:27 -0500

    Check first if BBB_ENV_SERVER_NAME exists before replacing MCONF_WEBCONF_SERVER and adding an entry to /etc/hosts with BBB_PORT_80_TCP_ADDR

--- mconf/run.sh
+++ mconf/run.sh
@@ -21,6 +21,12 @@ done
 
 #If neither of previous cases is true the installer will be run.
 echo -e "\n\e[92mStarting \e[0m Mconf \e[92mWeb Conferencing Platform !!\n\e[0m"
+if [ ! -z $BBB_ENV_SERVER_NAME ]; then
+  echo -e "Found linked BigBlueButton container, setting MCONF_WEBCONF_SERVER to: $BBB_ENV_SERVER_NAME" && MCONF_WEBCONF_SERVER=$BBB_ENV_SERVER_NAME
+  #We map BBB_ENV_SERVER_NAME to the IP address of the linked container so it
+  #doesn't resolve to the public IP address which isn't reachable from the container
+  sudo bash -c "printf '%s\t%s\n' $BBB_PORT_80_TCP_ADDR $BBB_ENV_SERVER_NAME | cat >> /etc/hosts"
+fi
 load_defaults
 #Finish mconf installation
 cd /var/www/mconf-web/
@@ -29,8 +35,7 @@ RAILS_ENV=production bundle exec rake secret:reset
 bundle exec rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile
 set_virtualhost_name "ServerName" $MCONF_SITE_DOMAIN
 [ "$MCONF_DISABLE_REGISTRATION" == "yes" ] && disable_registration
-[ ! -z $BBB_ENV_SERVER_NAME ] && echo -e "Found linked BigBlueButton container, setting MCONF_WEBCONF_SERVER to: $BBB_ENV_SERVER_NAME" && MCONF_WEBCONF_SERVER=$BBB_ENV_SERVER_NAME
-[ ! -z $BBB_ENV_SERVER_SALT ] && echo -e "Found linked BigBlueButton container, setting MCONF_WEBCONF_SALT to: $BBB_ENV_SERVER_SALT" && MCONF_WEBCONF_SALT=$BBB_ENV_SERVER_SALT
+
 #Launch supervisord
 echo -e "Starting supervisord..."
 sudo supervisord -c /etc/supervisor/supervisord.conf
